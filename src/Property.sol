4f7efda2f4153c8909e1df5bcf737faf2145dfd3
Bencooo <bentalha270@outlook.fr>
2025-01-14 14:51:37 +0100

    ERC721

--- src/Property.sol
+++ src/Property.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Property {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Property is ERC721 {
     
     string public name;
     string public symbol;
