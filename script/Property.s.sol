85957da11f6c2462c363c3185a730288a410e503
Bencooo <bentalha270@outlook.fr>
2025-01-16 13:32:38 +0100

    create script file to test the function

---
+++ script/Property.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {ImmoProperty} from "../src/Property.sol";
+
+contract PropertyScript is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        ImmoProperty immoProperty = new ImmoProperty("My property", "MPY");
+        console.log("ImmoProperty contract deployed at:", address(immoProperty));
+
+        immoProperty.createProperty(
+            "Villa",                           
+            "https://villa/image",        
+            1000,                                      
+            address(0x) 
+        );
+        
+        console.log("Get Property: ");
+        ImmoProperty.Property memory property1 = immoProperty.getProperty(1);
+        console.log("Property 1: Name:", property1.name);
+        console.log("Property 1: Price:", property1.price);
+        console.log("Property 1: Owner:", property1.owner);
+        console.log("Property 1: URI:", property1.uri);
+
+        vm.stopBroadcast();
+    }
+}
